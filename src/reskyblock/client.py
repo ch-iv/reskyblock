4cff9659c59b3e7fd972c273c8ac07b7dc84cf15
ch-iv <icheppy101@gmail.com>
2024-02-17 17:17:02 -0500

    feat: add bazaar

--- src/reskyblock/client.py
+++ src/reskyblock/client.py
@@ -1,7 +1,7 @@
 from reskyblock.http import AbstractHTTPClient, HTTPXClient
-from reskyblock.models import Auctions, AuctionsEnded
+from reskyblock.models import Auctions, AuctionsEnded, Bazaar
 from reskyblock.serialization import AbstractJSONDecoder, MSGSpecDecoder
-from reskyblock.urls import _prepare_auctions_ended_url, _prepare_auctions_url
+from reskyblock.urls import _prepare_auctions_ended_url, _prepare_auctions_url, _prepare_bazaar_url
 
 __all__ = ("Client",)
 
@@ -13,10 +13,15 @@ class Client:
 
     def get_auctions(self, page: int = 0) -> Auctions:
         """Get a single page of active auctions"""
-        rest_bytes = self._http_client.get(url=_prepare_auctions_url(page))
-        return self._json_decoder.serialize(rest_bytes, Auctions)
+        resp_bytes = self._http_client.get(url=_prepare_auctions_url(page))
+        return self._json_decoder.serialize(resp_bytes, Auctions)
 
     def get_auctions_ended(self) -> AuctionsEnded:
         """Get ended auctions"""
-        rest_bytes = self._http_client.get(url=_prepare_auctions_ended_url())
-        return self._json_decoder.serialize(rest_bytes, AuctionsEnded)
+        resp_bytes = self._http_client.get(url=_prepare_auctions_ended_url())
+        return self._json_decoder.serialize(resp_bytes, AuctionsEnded)
+
+    def get_bazaar(self) -> Bazaar:
+        """Get bazaar endpoint"""
+        resp_bytes = self._http_client.get(url=_prepare_bazaar_url())
+        return self._json_decoder.serialize(resp_bytes, Bazaar)
--- tests/test_client.py
+++ tests/test_client.py
@@ -8,6 +8,7 @@ from reskyblock import Client
 
 _AUCTIONS_DATA = (pathlib.Path(__file__).resolve().parents[0] / "data" / "auctions.json").read_text()
 _AUCTIONS_ENDED_DATA = (pathlib.Path(__file__).resolve().parents[0] / "data" / "auctions_ended.json").read_text()
+_BAZAAR_DATA = (pathlib.Path(__file__).resolve().parents[0] / "data" / "bazaar.json").read_text()
 
 
 @pytest.fixture
@@ -29,3 +30,9 @@ def test_get_auctions_ended(httpx_mock: HTTPXMock, client: Client) -> None:
     httpx_mock.add_response(json=json.loads(_AUCTIONS_ENDED_DATA))
 
     client.get_auctions_ended()
+
+
+def test_get_bazaar(httpx_mock: HTTPXMock, client: Client) -> None:
+    httpx_mock.add_response(json=json.loads(_BAZAAR_DATA))
+
+    client.get_bazaar()
