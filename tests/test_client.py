59c10739287e0a993874d4803b5fdc08abd4a3d2
ch-iv <icheppy101@gmail.com>
2024-02-17 17:06:53 -0500

    test: mock client api calls

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,13 @@
+import json
+import pathlib
+
 import pytest
+from pytest_httpx import HTTPXMock
 
 from reskyblock import Client
+
+_AUCTIONS_DATA = (pathlib.Path(__file__).resolve().parents[0] / "data" / "auctions.json").read_text()
+_AUCTIONS_ENDED_DATA = (pathlib.Path(__file__).resolve().parents[0] / "data" / "auctions_ended.json").read_text()
 
 
 @pytest.fixture
@@ -12,9 +19,13 @@ def test_client() -> None:
     _ = Client()
 
 
-def test_get_auctions(client: Client) -> None:
+def test_get_auctions(httpx_mock: HTTPXMock, client: Client) -> None:
+    httpx_mock.add_response(json=json.loads(_AUCTIONS_DATA))
+
     client.get_auctions()
 
 
-def test_get_auctions_ended(client: Client) -> None:
+def test_get_auctions_ended(httpx_mock: HTTPXMock, client: Client) -> None:
+    httpx_mock.add_response(json=json.loads(_AUCTIONS_ENDED_DATA))
+
     client.get_auctions_ended()
